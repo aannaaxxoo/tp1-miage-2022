55a07f4bb4d9ec036efa37ea85043c79a9d66f71
aannaaxxoo <32739116+aannaaxxoo@users.noreply.github.com>
2022-05-01 15:55:17 +0200

    exo 3

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,13 +1,29 @@
-package com.acme.todolist;
+package com.acme.todolist.domain;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TodolistApplicationTests {
 
+   @Test
+	void isLate_itemCreationDate24Hours_beforeCurrentTime_returnsFalse() {
+		TodoItem item = new TodoItem("1", Instant.now().minus(24, ChronoUnit.HOURS), "mycontent");
+		item.setContent(item.finalContent());
+		assertTrue(item.getContent().contains("LATE"));
+	}
+
 	@Test
-	void contextLoads() {
+	void isLate_itemCreationDate24Hours_afterCurrentTime_returnsFalse() {
+		TodoItem item = new TodoItem("1", Instant.now().plus(24, ChronoUnit.HOURS), "mycontent");
+
+		item.setContent(item.finalContent());
+		assertFalse(item.getContent().contains("LATE"));
 	}
 
 }
